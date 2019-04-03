3abd2ed0e8d8a02239be781b146ae49716e07d99
Bruno Contreras <bcontreras@ets.es>
2019-04-03 23:52:35 +0200

    Se mete el vídeo en la home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="home">
-    <p class="message">{{ message }}</p>
-    <div><button v-if="newUpdate" @click="click">Actualizar</button></div>
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-if="message || newUpdate" class="message">
+      <p>{{ message }}</p>
+      <button v-if="newUpdate" @click="click">Actualizar</button>
+    </div>
+    <video
+      ref="video"
+      src="@/assets/bg_video.mp4"
+      autoplay
+      muted
+      class="video"
+      loop
+      @canplay="$emit('loaded')"
+    ></video>
+    <div class="video-mask"></div>
   </div>
 </template>
 
 <script>
 import { ipcRenderer } from "electron";
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  },
   data: () => ({
     message: "",
     newUpdate: false
@@ -38,6 +43,29 @@ export default {
 
 <style>
 .message {
-  background-color: #eee;
+  background-color: rgba(255, 255, 255, 0.15);
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+  color: #fff;
+  max-width: 300px;
+  padding: 10px 40px;
+  position: absolute;
+  top: 40px;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+}
+.video {
+  height: 100vh;
+  position: fixed;
+  z-index: -1;
+  object-fit: cover;
+}
+.video-mask {
+  background-image: radial-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 1));
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  z-index: -1;
 }
 </style>
