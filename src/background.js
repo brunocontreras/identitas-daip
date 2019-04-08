46430e7910b126db70be4895d2f0c703b3592610
Bruno Contreras <bcontreras@ets.es>
2019-04-09 00:07:06 +0200

    Estructura para la generación de iconos

--- src/background.js
+++ src/background.js
@@ -21,8 +21,7 @@ protocol.registerStandardSchemes(["app"], { secure: true });
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    // fullscreen: true,
     webPreferences: {
       webSecurity: false
     }
