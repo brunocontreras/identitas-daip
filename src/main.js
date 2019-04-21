7ef629c1e39515e5b583a71a3e37ff201e8270ba
Bruno Contreras <bcontreras@ets.es>
2019-04-21 11:11:17 +0200

    Vue-material y vue-plyr en js independientes

--- src/main.js
+++ src/main.js
@@ -3,14 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import VueMaterial from "vue-material";
-import "vue-material/dist/vue-material.min.css";
-import "vue-material/dist/theme/default.css";
-Vue.use(VueMaterial);
-
-import VuePlyr from "vue-plyr";
-import "vue-plyr/dist/vue-plyr.css";
-Vue.use(VuePlyr);
+/* Plugins */
+import "@/plugins/vue-material";
+import "@/plugins/vue-plyr";
 
 Vue.config.productionTip = false;
 
