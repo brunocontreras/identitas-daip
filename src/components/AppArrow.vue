0274d45460b169b7e410b2de28afe84692a502ff
Bruno Contreras <bcontreras@ets.es>
2019-04-23 19:11:34 +0200

    Breadcrumb con arrow con svg

---
+++ src/components/AppArrow.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="15" height="80" viewBox="0 0 15 80">
+    <path d="M0,0 L 15,40 L 0,80" />
+  </svg>
+</template>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,10 +1,12 @@
 <template>
   <header class="theheader">
-    <the-logo class="logo bc-link" />
+    <the-logo class="logo bc-link bc-link--logo" />
+    <app-arrow class="arrow" />
     <app-breadcrumb v-if="current" v-slot="{ links }" class="breadcrumb" :item="current">
       <router-link v-for="(link, i) in links" :key="i" class="bc-link" :to="link.to">
         <span class="bc-link-name">{{ link.name }}</span>
       </router-link>
+      <app-arrow class="arrow" />
     </app-breadcrumb>
   </header>
 </template>
@@ -13,10 +15,12 @@
 import { mapState } from "vuex";
 import TheLogo from "@/components/TheLogo";
 import AppBreadcrumb from "@/components/AppBreadcrumb";
+import AppArrow from "@/components/AppArrow";
 export default {
   components: {
     TheLogo,
-    AppBreadcrumb
+    AppBreadcrumb,
+    AppArrow
   },
   computed: {
     ...mapState(["current"])
@@ -30,9 +34,7 @@ export default {
   --size: 80px;
   height: var(--size);
   background-color: #fff;
-  border: 1px solid var(--border-color);
-  border-left: none;
-  border-right: none;
+  margin-top: 1px;
   display: flex;
   align-items: stretch;
   box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
@@ -47,39 +49,29 @@ export default {
 .breadcrumb {
   flex-grow: 1;
   display: flex;
-  .bc-link {
-    padding-left: 4rem;
-  }
+}
+.bc-link--logo {
+  margin-left: 0;
+  padding-left: 2rem;
 }
 .bc-link {
   position: relative;
   display: flex;
   align-items: center;
   padding: 2rem;
-  padding-right: 2rem;
   font-size: 1.8rem;
-  &::before,
-  &::after {
-    content: "";
-    display: block;
-    border-right: 1px solid var(--border-color);
-    width: 1px;
-    height: 42px;
-    position: absolute;
-    left: 100%;
-  }
-  &::before {
-    top: 0;
-    transform: rotate(-20deg);
-    transform-origin: top;
-  }
-  &::after {
-    bottom: 0;
-    transform: rotate(20deg);
-    transform-origin: bottom;
-  }
+  padding-left: 4rem;
+  margin-left: -2rem;
+  transition-duration: 0s;
 }
 .bc-link-name {
   display: inline-block;
+}
+.arrow ::v-deep path {
+  position: relative;
+  z-index: 1;
+  stroke: var(--border-color);
+  stroke-width: 1;
+  fill: #fff;
 }
 </style>
